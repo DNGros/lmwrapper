bb54ceaecdb57998df5fb3b4a00c2eaa5d54717e
claudiosv <claudiosv@users.noreply.github.com>
2023-09-07 13:48:33 -0700

    Tear down

--- test/test_openai.py
+++ test/test_openai.py
@@ -17,15 +17,15 @@ def play_with_probs():
             logprobs=5,
             cache=True,
             num_completions=1,
-            echo=False
-        ))
+            echo=False,
+        )
+    )
     print(out)
     print(out._get_completion_token_index())
     print(out.completion_tokens)
     print(out.completion_token_offsets)
     print(out.completion_logprobs)
     print(out.prompt_tokens)
-
 
 
 def test_too_large_logprob():
@@ -40,7 +40,7 @@ def test_too_large_logprob():
                 logprobs=5,
                 cache=False,
                 num_completions=1,
-                echo=False
+                echo=False,
             )
         )
 
@@ -52,48 +52,48 @@ def test_too_large_logprob():
                 logprobs=10,
                 cache=False,
                 num_completions=1,
-                echo=False
+                echo=False,
             )
         )
 
 
-
-
 def test_simple_chat_mode():
     lm = get_open_ai_lm(OpenAiModelNames.gpt_3_5_turbo)
-    out = lm.predict(LmPrompt(
-        "What is 2+2? Answer with just one number.",
-        max_tokens=1,
-        num_completions=1,
-        cache=False,
-    ))
+    out = lm.predict(
+        LmPrompt(
+            "What is 2+2? Answer with just one number.",
+            max_tokens=1,
+            num_completions=1,
+            cache=False,
+        )
+    )
     assert out.completion_text.strip() == "4"
 
 
 def test_simple_chat_mode_multiturn():
     lm = get_open_ai_lm(OpenAiModelNames.gpt_3_5_turbo)
-    prompt = [
-        "What is 2+2? Answer with just one number.",
-        "4",
-        "What is 3+2?"
-    ]
+    prompt = ["What is 2+2? Answer with just one number.", "4", "What is 3+2?"]
     assert LmChatDialog(prompt).as_dicts() == [
-        {'role': 'user', 'content': 'What is 2+2? Answer with just one number.'},
-        {'role': 'assistant', 'content': '4'},
-        {'role': 'user', 'content': 'What is 3+2?'}
+        {"role": "user", "content": "What is 2+2? Answer with just one number."},
+        {"role": "assistant", "content": "4"},
+        {"role": "user", "content": "What is 3+2?"},
     ]
-    out = lm.predict(LmPrompt(
-        prompt,
-        max_tokens=1,
-        num_completions=1,
-        cache=False,
-    ))
+    out = lm.predict(
+        LmPrompt(
+            prompt,
+            max_tokens=1,
+            num_completions=1,
+            cache=False,
+        )
+    )
     assert out.completion_text.strip() == "5"
+
 
 def test_ratelimit():
     OpenAIPredictor.configure_global_ratelimit(1, per=2)
-    assert OpenAIPredictor._wait_ratelimit() == 0.
+    assert OpenAIPredictor._wait_ratelimit() == 0.0
     assert OpenAIPredictor._wait_ratelimit() == pytest.approx(2, rel=0.1)
+    OpenAIPredictor.configure_global_ratelimit(1000000, per=1)  # teardown
 
 
 def main():
@@ -104,15 +104,21 @@ def main():
     text = lm.predict(
         LmPrompt(
             "Once upon a time there was a Goose. And",
-            max_tokens=1, logprobs=10, cache=True
-        ))
+            max_tokens=1,
+            logprobs=10,
+            cache=True,
+        )
+    )
     print(text.completion_text)
     lm = get_open_ai_lm()
     new_text = lm.predict(
         LmPrompt(
             "Once upon a time there was a Goose. And",
-            max_tokens=1, logprobs=10, cache=True
-        ))
+            max_tokens=1,
+            logprobs=10,
+            cache=True,
+        )
+    )
     print(new_text.completion_text)
     assert text.completion_text == new_text.completion_text
     exit()
@@ -120,40 +126,46 @@ def main():
     text = lm.predict(
         LmPrompt(
             "Once upon a time there was a Goose. And",
-            max_tokens=1, logprobs=10,
-        ))
+            max_tokens=1,
+            logprobs=10,
+        )
+    )
     print(text.completion_text)
 
 
 def test_tokenizer_gpt3():
-    lm = get_open_ai_lm(
-        OpenAiModelNames.text_ada_001
-    )
+    lm = get_open_ai_lm(OpenAiModelNames.text_ada_001)
     assert lm.token_limit == 2049
     assert lm.estimate_tokens_in_prompt(LmPrompt("Once", max_tokens=10)) == 1
-    assert lm.estimate_tokens_in_prompt(LmPrompt(
-        "Once upon a time there was a magical place with a Spunctulus that was 3281 years old",
-        max_tokens=10
-    )) == 20
-    assert lm.estimate_tokens_in_prompt(LmPrompt(
-       " ".join(["once"] * 2000), max_tokens=10
-    )) == 2000
-    assert not lm.could_completion_go_over_token_limit(LmPrompt(
-        " ".join(["once"] * 2000), max_tokens=10
-    ))
-    assert lm.could_completion_go_over_token_limit(LmPrompt(
-        " ".join(["once"] * 2000), max_tokens=50
-    ))
+    assert (
+        lm.estimate_tokens_in_prompt(
+            LmPrompt(
+                "Once upon a time there was a magical place with a Spunctulus that was 3281 years old",
+                max_tokens=10,
+            )
+        )
+        == 20
+    )
+    assert (
+        lm.estimate_tokens_in_prompt(LmPrompt(" ".join(["once"] * 2000), max_tokens=10))
+        == 2000
+    )
+    assert not lm.could_completion_go_over_token_limit(
+        LmPrompt(" ".join(["once"] * 2000), max_tokens=10)
+    )
+    assert lm.could_completion_go_over_token_limit(
+        LmPrompt(" ".join(["once"] * 2000), max_tokens=50)
+    )
 
 
 def test_tokenizer_chat():
-    lm = get_open_ai_lm(
-        OpenAiModelNames.gpt_3_5_turbo
-    )
+    lm = get_open_ai_lm(OpenAiModelNames.gpt_3_5_turbo)
     assert lm.token_limit == 4096
-    assert 5 < lm.estimate_tokens_in_prompt(LmPrompt(
-        "Once upon a time", max_tokens=10
-    )) < 4 + 12
+    assert (
+        5
+        < lm.estimate_tokens_in_prompt(LmPrompt("Once upon a time", max_tokens=10))
+        < 4 + 12
+    )
 
 
 if __name__ == "__main__":
